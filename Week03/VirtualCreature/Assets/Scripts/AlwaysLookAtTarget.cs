6e7b62795fafbc37ed1f6197b84c55480474ed4d
Michael J. Young <michaeljyoung@gmail.com>
2024-02-24 18:05:52 -0500

    LookAt Target
    Implemented script so the drone always looks at the cube.

---
+++ Week03/VirtualCreature/Assets/Scripts/AlwaysLookAtTarget.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script credit LlamAcademy
+
+public class AlwaysLookAtTarget : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
